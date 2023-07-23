76cde3657696ec6c294c8764005dfbf8eafe0c54
I73v <i73v@pm.me>
2023-07-23 12:24:04 -0400

    Added Dockerfile
    [skip ci]

---
+++ docker/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+SHORT=l
+LONG=llvm
+OPTS=$(getopt -o $SHORT: --long $LONG: -- "$@")
+
+eval set -- "$OPTS"
+
+LLVM=
+
+while true; do
+  case "$1" in
+    -l | --llvm ) LLVM="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+if [ -z "$LLVM" ]; then
+  echo "-l | --llvm is undefined"
+  exit 1
+fi
+
+echo "Installing necessary packages from standard repositories"
+apt-get update -qq && export DEBIAN_FRONTEND=noninteractive && \
+    apt-get install -y --no-install-recommends software-properties-common apt-utils wget curl file gpg \
+        build-essential ccache git python3 python3-pip xorriso ninja-build
+
+
+echo "Installing Meson"
+pip3 install --user meson
+
+echo "Installing Taskfile"
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
+
+echo "Installing LLVM"
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+./llvm.sh "$LLVM" all
